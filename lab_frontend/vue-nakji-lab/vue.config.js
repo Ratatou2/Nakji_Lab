f3cf5f98e8279c01e5fe4da26ffc4e2e9e2b6376
jinak <ws423257@gmail.com>
2024-11-12 16:07:02 +0900

    [base] Vue 프로젝트 분리

--- lab_frontend/vue-nakji-lab/vue.config.js
+++ lab_frontend/vue-nakji-lab/vue.config.js
@@ -1,17 +1,4 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: "../../lab_backend/src/main/resources/static/",
-  devServer: {
-    port: 8081, // 접속 포트 변경
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        pathRewrite: {
-          "^/api": "",
-        },
-      },
-    },
-  },
-});
+  transpileDependencies: true
+})
