4e49f95073faa1d5443d5f6e11bb08d69fbe377e
jinak <ws423257@gmail.com>
2024-11-13 09:33:06 +0900

    [fix] index.html 못 찾는 에러

--- lab_frontend/vue-nakji-lab/vue.config.js
+++ lab_frontend/vue-nakji-lab/vue.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: "../../lab_backend/src/main/resources/static/", // 빌드된 파일을 static에 저장
-  indexPath: "index.html", // 출력되는 HTML 파일 이름을 명시
   devServer: {
     port: 8081, // Vue 개발 서버 포트
     proxy: {
