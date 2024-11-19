d4290eec4c857f9808b25df60f3d325253358bf7
jinak <ws423257@gmail.com>
2024-11-19 13:33:56 +0900

    [fix] 전역 CSS 적용 안되는 이슈 수정

--- lab_frontend/vue-nakji-lab/src/main.js
+++ lab_frontend/vue-nakji-lab/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./assets/css/sweetalert2.css";
-import "./assets/css/main.css";
+import "@/assets/css/main.css";
+import "@/assets/css/sweetalert2.css";
 
 createApp(App).mount("#app");
