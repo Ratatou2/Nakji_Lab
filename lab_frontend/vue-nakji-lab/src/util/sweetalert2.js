6c55659ab79d12f141641bfb39075a229dce3997
jinak <ws423257@gmail.com>
2024-11-19 16:35:14 +0900

    [fix] 전역 CSS 적용 안되는 이슈 수정

--- lab_frontend/vue-nakji-lab/src/util/sweetalert2.js
+++ lab_frontend/vue-nakji-lab/src/util/sweetalert2.js
@@ -1,19 +1,19 @@
-import Swal from "sweetalert2";
+// import Swal from "sweetalert2";
 
-const Toast = Swal.mixin({
-  toast: true,
-  position: "top-end",
-  iconColor: "white",
-  customClass: {
-    popup: "colored-toast",
-  },
-  showConfirmButton: false,
-  timer: 4500,
-  timerProgressBar: true,
-  didOpen: (toast) => {
-    toast.addEventListener("mouseenter", Swal.stopTimer);
-    toast.addEventListener("mouseleave", Swal.resumeTimer);
-  },
-});
+// const Toast = Swal.mixin({
+//   toast: true,
+//   position: "top-end",
+//   iconColor: "white",
+//   customClass: {
+//     popup: "colored-toast",
+//   },
+//   showConfirmButton: false,
+//   timer: 4500,
+//   timerProgressBar: true,
+//   didOpen: (toast) => {
+//     toast.addEventListener("mouseenter", Swal.stopTimer);
+//     toast.addEventListener("mouseleave", Swal.resumeTimer);
+//   },
+// });
 
-export default Toast;
+// export default Toast;
